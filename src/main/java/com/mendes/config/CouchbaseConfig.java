6383a9bc5aef3a547e8f9db2b92c8a99e2234b4a
mendesmustafa <mendesmustafa012@gmail.com>
2021-04-01 12:41:17 +0300

    updated CouchbaseConfig class

--- src/main/java/com/mendes/config/CouchbaseConfig.java
+++ src/main/java/com/mendes/config/CouchbaseConfig.java
@@ -1,8 +1,18 @@
 package com.mendes.config;
 
+import com.mendes.entity.Task;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.couchbase.CouchbaseClientFactory;
+import org.springframework.data.couchbase.SimpleCouchbaseClientFactory;
 import org.springframework.data.couchbase.config.AbstractCouchbaseConfiguration;
+import org.springframework.data.couchbase.core.CouchbaseTemplate;
+import org.springframework.data.couchbase.core.convert.CouchbaseCustomConversions;
+import org.springframework.data.couchbase.repository.config.RepositoryOperationsMapping;
+
+import java.util.Collections;
 
 /**
  * @author mendes
@@ -10,6 +20,9 @@ import org.springframework.data.couchbase.config.AbstractCouchbaseConfiguration;
 
 @Configuration
 public class CouchbaseConfig extends AbstractCouchbaseConfiguration {
+
+    @Autowired
+    private ApplicationContext applicationContext;
 
     @Value("${spring.couchbase.connection-string}")
     private String connection;
@@ -41,5 +54,30 @@ public class CouchbaseConfig extends AbstractCouchbaseConfiguration {
     @Override
     public String getBucketName() {
         return this.bucketName;
+    }
+
+    @Override
+    public void configureRepositoryOperationsMapping(RepositoryOperationsMapping mapping) {
+
+        try {
+            mapping.mapEntity(Task.class, getCouchbaseTemplate("task"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private CouchbaseTemplate getCouchbaseTemplate(String bucketName) throws Exception {
+
+        CouchbaseTemplate template = new CouchbaseTemplate(couchbaseClientFactory(bucketName),
+                mappingCouchbaseConverter(couchbaseMappingContext(customConversions()),
+                        new CouchbaseCustomConversions(Collections.emptyList())));
+        template.setApplicationContext(applicationContext);
+
+        return template;
+    }
+
+    private CouchbaseClientFactory couchbaseClientFactory(String bucketName) {
+
+        return new SimpleCouchbaseClientFactory(couchbaseCluster(couchbaseClusterEnvironment()), bucketName, this.getScopeName());
     }
 }

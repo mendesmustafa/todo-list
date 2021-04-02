0f0fe88b3e05da99a0e50d652139049e6dcb3d26
mendesmustafa <mendesmustafa012@gmail.com>
2021-04-03 00:25:42 +0300

    created SwaggerConfig class and updated UserController, TaskController

--- src/main/java/com/mendes/controller/TaskController.java
+++ src/main/java/com/mendes/controller/TaskController.java
@@ -2,6 +2,10 @@ package com.mendes.controller;
 
 import com.mendes.entity.Task;
 import com.mendes.service.TaskService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * @author mendes
  */
 
+@Api(value = "task")
 @Controller
 @RequestMapping("/task")
 public class TaskController {
@@ -23,11 +28,25 @@ public class TaskController {
         this.taskService = taskService;
     }
 
+    @ApiOperation(value = "save")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Success|OK"),
+            @ApiResponse(code = 400, message = "Bad Request"),
+            @ApiResponse(code = 404, message = "not found!!!"),
+            @ApiResponse(code = 500, message = "Internal Server Error")
+    })
     @PostMapping("/save")
     public ResponseEntity save(@RequestBody Task model) {
         return ResponseEntity.ok(taskService.save(model));
     }
 
+    @ApiOperation(value = "list")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Success|OK"),
+            @ApiResponse(code = 400, message = "Bad Request"),
+            @ApiResponse(code = 404, message = "not found!!!"),
+            @ApiResponse(code = 500, message = "Internal Server Error")
+    })
     @GetMapping("/list")
     public ResponseEntity list() {
         return ResponseEntity.ok(taskService.list());
--- src/main/java/com/mendes/controller/UserController.java
+++ src/main/java/com/mendes/controller/UserController.java
@@ -2,6 +2,10 @@ package com.mendes.controller;
 
 import com.mendes.entity.User;
 import com.mendes.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
  * @author mendes
  */
 
+@Api(value = "user")
 @RestController
 @RequestMapping("/user")
 public class UserController {
@@ -19,11 +24,25 @@ public class UserController {
         this.userService = userService;
     }
 
+    @ApiOperation(value = "save")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Success|OK"),
+            @ApiResponse(code = 400, message = "Bad Request"),
+            @ApiResponse(code = 404, message = "not found!!!"),
+            @ApiResponse(code = 500, message = "Internal Server Error")
+    })
     @PostMapping("/save")
     public ResponseEntity save(@RequestBody User model) {
         return ResponseEntity.ok(userService.save(model));
     }
 
+    @ApiOperation(value = "list")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Success|OK"),
+            @ApiResponse(code = 400, message = "Bad Request"),
+            @ApiResponse(code = 404, message = "not found!!!"),
+            @ApiResponse(code = 500, message = "Internal Server Error")
+    })
     @GetMapping("/list")
     public ResponseEntity list() {
         return ResponseEntity.ok(userService.list());
